c9018fc86bb3f4281569f05d7f66ef9441eaaa9c
Jake Fletcher <jacobsfletch@gmail.com>
2026-07-15 12:49:22 -0400

    feat: initial commit

---
+++ src/payload.config.ts
@@ -0,0 +1,42 @@
+import { sqliteAdapter } from '@payloadcms/db-sqlite'
+import { mcpPlugin } from '@payloadcms/plugin-mcp'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import path from 'path'
+import { buildConfig } from 'payload'
+import { fileURLToPath } from 'url'
+import sharp from 'sharp'
+
+import { Users } from './collections/Users'
+import { Media } from './collections/Media'
+import { Folders } from './collections/Folders'
+import { Tags } from './collections/Tags'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    importMap: {
+      baseDir: path.resolve(dirname),
+    },
+  },
+  collections: [Users, Media, Folders, Tags],
+  editor: lexicalEditor(),
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  db: sqliteAdapter({
+    client: {
+      url: process.env.DATABASE_URL || '',
+    },
+  }),
+  sharp,
+  localization: {
+    locales: ['en'],
+    fallback: true,
+    defaultLocale: 'en',
+  },
+  plugins: [mcpPlugin({})],
+})
